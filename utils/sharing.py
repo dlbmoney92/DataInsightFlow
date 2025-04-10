4fa232dce893a347429ce1d859b8af8bb87125e5
Analytics Assist <analytics-assist@example.com>
2025-04-10 16:18:08 +0000

    Initial commit of Analytics Assist application

--- utils/sharing.py
+++ utils/sharing.py
@@ -161,23 +161,45 @@ def generate_share_card(title, content_type, share_link, include_social=True, su
             </style>
             """, unsafe_allow_html=True)
             
-            # Generate HTML for social buttons
+            # Using SVG logos for social media buttons
+            
+            # LinkedIn SVG logo
+            linkedin_logo = """<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="#0077b5">
+                <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
+            </svg>"""
+
+            # Twitter/X SVG logo
+            twitter_logo = """<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="#1DA1F2">
+                <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
+            </svg>"""
+
+            # Facebook SVG logo
+            facebook_logo = """<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="#4267B2">
+                <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" />
+            </svg>"""
+
+            # Email SVG logo
+            email_logo = """<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="#555555">
+                <path d="M12 12.713l-11.985-9.713h23.97l-11.985 9.713zm0 2.574l-12-9.725v15.438h24v-15.438l-12 9.725z" />
+            </svg>"""
+            
+            # Generate HTML for social buttons with SVG logos
             html = """
             <div class="social-buttons">
                 <a href="{linkedin}" target="_blank" class="social-button" title="Share on LinkedIn">
-                    <div class="social-icon">🔗</div>
+                    <div class="social-icon">{linkedin_logo}</div>
                     <div class="social-name">LinkedIn</div>
                 </a>
                 <a href="{twitter}" target="_blank" class="social-button" title="Share on Twitter">
-                    <div class="social-icon">🐦</div>
+                    <div class="social-icon">{twitter_logo}</div>
                     <div class="social-name">Twitter</div>
                 </a>
                 <a href="{facebook}" target="_blank" class="social-button" title="Share on Facebook">
-                    <div class="social-icon">📘</div>
+                    <div class="social-icon">{facebook_logo}</div>
                     <div class="social-name">Facebook</div>
                 </a>
                 <a href="{email}" target="_blank" class="social-button" title="Share via Email">
-                    <div class="social-icon">✉️</div>
+                    <div class="social-icon">{email_logo}</div>
                     <div class="social-name">Email</div>
                 </a>
             </div>
@@ -185,7 +207,11 @@ def generate_share_card(title, content_type, share_link, include_social=True, su
                 linkedin=social_links["linkedin"],
                 twitter=social_links["twitter"],
                 facebook=social_links["facebook"],
-                email=social_links["email"]
+                email=social_links["email"],
+                linkedin_logo=linkedin_logo,
+                twitter_logo=twitter_logo,
+                facebook_logo=facebook_logo,
+                email_logo=email_logo
             )
             
             st.markdown(html, unsafe_allow_html=True)
